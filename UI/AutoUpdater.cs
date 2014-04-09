08693cfb7e7374fe0c9073d5c6ad9d1dfe2d0516
villagra.gaston@gmail.com <villagra.gaston@gmail.com@4e7e00e1-5e99-0197-c487-98621953d519>
2014-04-09 01:38:23 +0000

    Archivos para el AutoUpdater

---
+++ UI/AutoUpdater.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+using SharpUpdate;
+
+namespace UpdateApp
+{
+    public partial class AutoUpdater : Form, ISharpUpdatable
+    {
+        private SharpUpdater updater;
+
+        public AutoUpdater()
+        {
+            InitializeComponent();
+
+            this.label1.Text = this.ApplicationAssembly.GetName().Version.ToString();
+            updater = new SharpUpdater(this);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            updater.DoUpdate();
+        }
+
+        #region SharpUpdate
+        public string ApplicationName
+        {
+            get { return "TestApp"; }
+        }
+
+        public string ApplicationID
+        {
+            get { return "TestApp"; }
+        }
+
+        public Assembly ApplicationAssembly
+        {
+            get { return Assembly.GetExecutingAssembly(); }
+        }
+
+        public Icon ApplicationIcon
+        {
+            get { return this.Icon; }
+        }
+
+        public Uri UpdateXmlLocation
+        {
+            get { return new Uri("http://www.stonerbotbd.co.nf/update.xml"); }
+        }
+
+        public Form Context
+        {
+            get { return this; }
+        }
+        #endregion
+
+        private void Close2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
